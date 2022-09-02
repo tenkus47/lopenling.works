dd9bb7102a4cc0201d3c91940e8f224b0ab404ca
Tenzin Kunsang <tenkus47@gmail.com>
2022-09-02 11:00:40 +0530

    added pagination on homepage

--- app/components/HomePage/HomePage.js
+++ app/components/HomePage/HomePage.js
@@ -20,21 +20,34 @@ import {
   FormControlLabel,
   Checkbox,
   Divider,
+  Pagination,
 } from "@mui/material";
 import Tooltip from "@mui/material/Tooltip";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import Filter from "./Filter";
 import { motion } from "framer-motion";
 import Footer from "./Footer";
+import usePagination from "./pagination";
+const PER_PAGE = 7;
+
 function HomePage(props) {
   let { Textdata } = props;
   let { activeText, detail } = Textdata;
+  let [page, setPage] = useState(1);
   const [filteredData, setFilteredData] = useState([]);
   const [showFilter, setShowFilter] = useState(false);
   const [filteredTitle, setFilteredTitle] = useState("");
   // const [categorySelected, setCategorySelected] = useState([]);
   // const [author, setAuthor] = useState([]);
   // const [lengthRange, setLengthRange] = useState([]);
+  const count = Math.ceil(filteredData.length / PER_PAGE);
+  let _DATA = usePagination(filteredData, PER_PAGE) || [];
+
+  const handleChange = (e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+    console.log(_DATA);
+  };
 
   const handleToggleFilter = React.useCallback(() => {
     setShowFilter((prev) => !prev);
@@ -70,13 +83,6 @@ function HomePage(props) {
     setFilteredData(tempData.sort((a, b) => a?.order - b?.order));
   }, [filteredTitle, detail]);
 
-  function handleLeft() {
-    listRef.current.scrollLeft -= 320;
-  }
-  function handleRight() {
-    listRef.current.scrollLeft += 320;
-  }
-
   return (
     <Stack sx={{ height: "100vh" }}>
       <Box
@@ -104,81 +110,96 @@ function HomePage(props) {
           // setAuthor={setAuthor}
         />
       </Collapse>
-      <Box
-        mt={2}
-        py={1}
-        display="flex"
-        // height={{ xs: "60vh", md: "auto" }}
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
       >
-        <IconButton onClick={handleLeft} disableRipple style={{ flex: 1 }}>
-          <KeyboardDoubleArrowLeftIcon />
-        </IconButton>
-        <motion.div layout style={{ flex: 1, maxWidth: "80%" }}>
-          <Stack
-            sx={{
-              overflowX: "auto",
-              scrollSnapType: "x mandatory",
-            }}
-            p={1}
-            pb={2}
-            direction="row"
-            className={styles.list}
-            ref={listRef}
-          >
-            {filteredData.map((pecha, i) => {
-              return (
-                <Box
-                  key={pecha.id || `filteredData-${i}`}
-                  px={1}
-                  sx={{
-                    width: "auto",
-                    scrollSnapAlign: "start",
-                  }}
-                >
-                  <motion.div
-                    animate={{ opacity: 1 }}
-                    initial={{ opacity: 0 }}
-                    exit={{ opacity: 0 }}
-                    layout
+        {filteredData.length > PER_PAGE && (
+          <Pagination
+            count={count}
+            size="small"
+            page={page}
+            variant="outlined"
+            shape="rounded"
+            onChange={handleChange}
+            autoCapitalize
+            sx={{ alignSelf: "flex-end", marginTop: 2 }}
+          />
+        )}
+        <Box
+          mt={2}
+          py={1}
+          display="flex"
+          // height={{ xs: "60vh", md: "auto" }}
+        >
+          <motion.div layout>
+            <Stack
+              sx={{
+                flexWrap: "wrap",
+                gap: 1,
+              }}
+              p={1}
+              pb={2}
+              direction="row"
+              className={styles.list}
+              ref={listRef}
+            >
+              {_DATA.currentData().map((pecha, i) => {
+                return (
+                  <Box
+                    key={pecha.id || `filteredData-${i}`}
+                    px={1}
+                    sx={{
+                      width: "auto",
+                      scrollSnapAlign: "start",
+                    }}
                   >
-                    <Link
-                      to={`/texts/${pecha.text}`}
-                      style={{ textDecoration: "none" }}
+                    <motion.div
+                      animate={{ opacity: 1 }}
+                      initial={{ opacity: 0 }}
+                      exit={{ opacity: 0 }}
+                      layout
                     >
-                      <Card
-                        sx={{
-                          cursor: "pointer",
-                          textDecoration: "none",
-                        }}
-                        elevation={3}
-                        key={pecha.id}
+                      <Link
+                        to={`/texts/${pecha.text}`}
+                        style={{ textDecoration: "none" }}
                       >
-                        <Tooltip title={pecha.description}>
-                          <CardContent>
-                            <Typography
-                              gutterBottom
-                              variant="h6"
-                              textAlign="center"
-                              textTransform="capitalize"
-                              fontWeight="bold"
-                              width="max-content"
-                            >
-                              {addTibetanShay(pecha.title)}
-                            </Typography>
-                          </CardContent>
-                        </Tooltip>
-                      </Card>
-                    </Link>
-                  </motion.div>
-                </Box>
-              );
-            })}
-          </Stack>
-        </motion.div>
-        <IconButton onClick={handleRight} disableRipple style={{ flex: 1 }}>
-          <KeyboardDoubleArrowRightIcon />
-        </IconButton>
-      </Box>
+                        <Card
+                          sx={{
+                            cursor: "pointer",
+                            textDecoration: "none",
+                          }}
+                          elevation={3}
+                          key={pecha.id}
+                        >
+                          <Tooltip title={pecha.description}>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h6"
+                                textAlign="center"
+                                textTransform="capitalize"
+                                fontWeight="bold"
+                                width="max-content"
+                              >
+                                {addTibetanShay(pecha.title)}
+                              </Typography>
+                            </CardContent>
+                          </Tooltip>
+                        </Card>
+                      </Link>
+                    </motion.div>
+                  </Box>
+                );
+              })}
+            </Stack>
+          </motion.div>
+        </Box>
+      </Container>
       <Footer />
     </Stack>
   );
